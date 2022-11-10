a0a11cb0712ac37d1afbe0b59f7ea3fe2b2bbeb7
rokobata510 <rokobata510@gmail.com>
2022-11-10 16:54:41 +0100

    fixed formatting

--- example-app/resources/views/myview.blade.php
+++ example-app/resources/views/myview.blade.php
@@ -13,6 +13,7 @@
         <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
         <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.3.1/dist/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
 
+
     </head>
     <body>
         <!-- Navigation-->
