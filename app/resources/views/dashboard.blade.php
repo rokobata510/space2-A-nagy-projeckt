964a24950499f578699166ec3cd9bc3c27f0779e
Timelya <petrovicstimi931@gmail.com>
2022-12-05 14:27:25 +0100

    Fő oldal szerkesztve

--- app/resources/views/dashboard.blade.php
+++ app/resources/views/dashboard.blade.php
@@ -6,19 +6,46 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Home</title>
 </head>
-<body>
+<body >
 
-    <x-app-layout >
+    <x-app-layout>
+        <br>
+        <br>
+        <table class="table-auto min-w-full flex flex-col items-center">
+            <tbody>
+              <tr>
+                <td class="table-auto min-w-full flex flex-col items-center">
+                <img class="min-w-auto flex flex-col items-center" src="{{url('/ron.jpg')}}" alt="meme">
+                </td>
+              </tr>
+              <tr>
+                <td style="font-size: 50px " class="table-auto min-w-full flex flex-col items-center text-pink-600
+                text-center font-bold md:font-Inter  ..."> Mi van a képen? </td>
+              </tr>
+              <tr>
+                <td>
+                    <form>
+                        <x-input-label class="block">
+                            <div class="bg-[#1E1E1E]">
+                                <x-input-label style="font-size: 17px" class="md:font-Inter text-black ..." for="text" />
+                                <x-text-input id="text" class="text-pink-900 block mt-1 w-full border-none ..."
+                                type="text" name="text" required autofocus />
+                                <hr class="my-0  h-px bg-[#1E1E1E] rounded border-0 ">
+                            </div>
+                        </x-input-label>
+                      </form>
+                </td>
+                <td>
+                    <x-primary-button type="button" style="font-size: 20px "
+                    class="focus:outline-none focus:ring-5 bg-gradient-to-r
+                     from-pink-700 to-fuchsia-800 hover:from-violet-800 hover:to-indigo-500 ...">
+                        Küldés
+                    </x-primary-button>
+                </td>
+              </tr>
+            </tbody>
+          </table>
 
-        <div class="">
-            <div class="mx-auto ssm:px-6 ">
-                <div class="overflow-hidden shadow-sm">
-                    <div class="p-6 text-gray-900 ">
-                        {{ __("You're logged in!") }}
-                    </div>
-                </div>
-            </div>
-        </div>
     </x-app-layout>
 
 </body>
--- app/resources/views/layouts/app.blade.php
+++ app/resources/views/layouts/app.blade.php
@@ -14,7 +14,7 @@
         @vite(['resources/css/app.css', 'resources/js/app.js'])
     </head>
     <body class="font-sans antialiased">
-        <div class="min-h-screen bg-gray-100">
+        <div class="min-h-screen bg-[#1E1E1E]">
             @include('layouts.navigation')
 
             <!-- Page Heading -->
