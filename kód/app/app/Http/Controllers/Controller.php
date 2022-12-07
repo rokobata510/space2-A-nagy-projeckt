b864e18d818504802bb3f15629ad6c6c48f7ac4f
Les <77836486+lesheidrich@users.noreply.github.com>
2022-12-07 22:27:44 +0200

    Controller checkAdmin funkcio alap

--- kód/app/app/Http/Controllers/Controller.php
+++ kód/app/app/Http/Controllers/Controller.php
@@ -8,9 +8,16 @@ use Illuminate\Foundation\Bus\DispatchesJobs;
 use Illuminate\Foundation\Validation\ValidatesRequests;
 use Illuminate\Routing\Controller as BaseController;
 use Symfony\Component\HttpFoundation\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Redirect;
+
 
 class Controller extends BaseController
 {
     use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
+    public static function checkAdmin()
+    {
+        // -> returnolja redirect cimet kesobbi hasznalatra
+    }
     
 }
